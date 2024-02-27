1155769e14ec6d057e94d1bbae46f656c6e0146d
sandeep <sandeep@gmail.com>
2024-02-27 09:32:24 +0530

    sa

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Locsk-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
