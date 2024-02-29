1a1bb3a5f019ec22139b903a9e340a06322466c7
sandeep <sandeep@gmail.com>
2024-02-29 10:11:23 +0530

    s

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tire4"
+    bucket         = "tire3s3"
     region         = "us-west-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "tire3-ke"
+    dynamodb_table = "tire3"
     encrypt        = true
   }
   required_version = ">=0.13.0"
